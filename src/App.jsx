dd56f704fc4e2c515e7e08e409f5726931f30372
Paulina <fpaulina545@gmail.com>
2024-10-11 20:08:32 -0500

    Instalación de libreria React Icons para el complemento del componente ToolsBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import ToolsBar from './components/Tools-bar/ToolsBar';
 
 function App() {
   return (
     <>
       <div>
         <h1>POST UI</h1>
+        <ToolsBar>
+
+        </ToolsBar>
       </div>
     </>
   );
