e427c32c635f859de9ff492a4340c22078276bef
Issblann <anaisabelpp@outlook.com>
2024-10-11 21:11:13 -0500

    agrego component post card

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { PostUI } from './components/post';
 import './App.css';
+import { PostCard } from './components/post-card/post-card';
 export const Layout = () => {
   return (
     <main>
       <h1>HOME</h1>
-      <PostUI />
+      <PostCard />
       <span className="layout--separator"></span>
       <PostUI />
       <span className="layout--separator"></span>
---
+++ src/components/post-card/post-card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PostUI } from '../post';
+import ToolsBar from '../Tools-bar/ToolsBar';
+
+export const PostCard = () => {
+  return (
+    <div>
+      <PostUI />
+      <ToolsBar />
+    </div>
+  );
+};
