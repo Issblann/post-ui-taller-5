13fc1a8b8e80667e760577dbe0c88c5ca6ef4cfa
Paulina <fpaulina545@gmail.com>
2024-10-14 14:04:29 -0500

    Llamado a la API y manejo de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await response.json();
+        setPosts(data.slice(0, 16));
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts(); 
+  }, []);
+
   return (
-    <>
+    <div>
+      <h1>Posts</h1>
       <div>
-        <h1>POST UI</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default App;
+
+
+
